a843c92d35dbd42b551e72a351a560066a8890cd
Matheus Braynner <matheus.souza@telefonicati.onmicrosoft.com>
2023-12-10 19:34:13 -0300

    Making a polygon and some transformations

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,7 @@
+
+=
+src/Main.java,9\2\924f089edb4af5ee45c8a9d1129cd28e2b368138
+@
+src/MyFrame.java,5\4\54054345f2147041e18e06a485612d75373bb074
+@
+src/MyPanel.java,f\e\fec1024e0c7207e57c2fb5de4cfaa922bdeda007
--- src/MyPanel.java
+++ src/MyPanel.java
@@ -1,27 +1,24 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.geom.AffineTransform;
 
 public class MyPanel extends JPanel {
 
-    //Image image;
-
     MyPanel() {
-
-        //image = new ImageIcon("sky.png").getImage();
-        this.setPreferredSize(new Dimension(640, 480));
+        this.setPreferredSize(new Dimension(800, 600));
     }
 
     public void paint(Graphics g) {
 
         Graphics2D g2D = (Graphics2D) g;
 
-        int width = 640;
-        int height = 480;
+        int width = 800;
+        int height = 600;
         int pointSize = 8;
         String coordinateX = "", coordinateY = "";
 
         int limite = (width / (width / 100));
-        int space = width/30;
+        int scale = 50;
         int valueCoordinate = 0;
 
         g2D.setPaint(Color.black);
@@ -29,37 +26,73 @@ public class MyPanel extends JPanel {
         g2D.drawLine(0, (height / 2), width, (height / 2));
         g2D.drawLine((width / 2), 0, (width / 2), height);
 
-
-        g2D.setPaint(Color.blue);
+        g2D.setPaint(Color.black);
         g2D.fillOval(((width / 2) - (pointSize / 2)), ((height / 2) - (pointSize / 2)), pointSize, pointSize);
 
         // right side
         for (int i = 0; i < limite; i++) {
-            g2D.fillOval((width / 2) - (pointSize / 2) + space, (height / 2) - (pointSize / 2), pointSize, pointSize);
-            g2D.fillOval((width / 2) - (pointSize / 2), (height / 2) - (pointSize / 2) + space, pointSize, pointSize);
+            g2D.setPaint(Color.black);
+            g2D.fillOval((width / 2) - (pointSize / 2) + scale, (height / 2) - (pointSize / 2), pointSize, pointSize);
+            g2D.fillOval((width / 2) - (pointSize / 2), (height / 2) - (pointSize / 2) + scale, pointSize, pointSize);
             coordinateX = coordinateY = String.valueOf(valueCoordinate + 1);
             // Letters
-            g2D.drawString(coordinateX, (width / 2) - (pointSize / 2) + space, (height / 2) + (pointSize * 4));
-            g2D.drawString("-" + coordinateY, (width / 2) - (pointSize * 4), (height / 2) + (pointSize + space));
+            g2D.setPaint(Color.blue);
+            g2D.drawString(coordinateX, (width / 2) - (pointSize / 2) + scale, (height / 2) + (pointSize * 4));
+            g2D.drawString("-" + coordinateY, (width / 2) - (pointSize * 4), (height / 2) + (pointSize + scale));
             // Increment Variables
             valueCoordinate++;
-            space += width / 30;
+            scale += 50;
         }
 
-        space = width / 30;
+        scale = 50;
         valueCoordinate = 0;
         for (int i = limite; i > 0; i--) {
-            // Ovals
-            g2D.fillOval((width / 2) - (pointSize / 2) - space, (height / 2) - (pointSize / 2), pointSize, pointSize);
-            g2D.fillOval((width / 2) - (pointSize / 2), (height / 2) - (pointSize / 2) - space, pointSize, pointSize);
+            g2D.setPaint(Color.black);
+            g2D.fillOval((width / 2) - (pointSize / 2) - scale, (height / 2) - (pointSize / 2), pointSize, pointSize);
+            g2D.fillOval((width / 2) - (pointSize / 2), (height / 2) - (pointSize / 2) - scale, pointSize, pointSize);
             coordinateX = coordinateY = String.valueOf(valueCoordinate + 1);
             // Letters
-            if (i != limite)
-                g2D.drawString("-" + coordinateX, (width / 2) - (pointSize / 2) - space, (height / 2) + (pointSize * 4));
-                g2D.drawString(coordinateY, (width / 2) - (pointSize * 4), (height / 2) + (pointSize - space));
+            g2D.setPaint(Color.blue);
+            g2D.drawString("-" + coordinateX, (width / 2) - (pointSize / 2) - scale, (height / 2) + (pointSize * 4));
+            g2D.drawString(coordinateY, (width / 2) - (pointSize * 4), (height / 2) + (pointSize - scale));
             // Increment Variables
             valueCoordinate++;
-            space += width / 30;
+            scale += 50;
         }
+
+        // points to draw a polygon
+        int[] xPoints = {141, 150, 166, 175, 175, 166, 150, 141};
+        int[] yPoints = {75, 79, 79, 75, 62, 50, 50, 62};
+
+        AffineTransform transform = new AffineTransform();
+
+        Shape scaledPolygon = transform.createTransformedShape(new Polygon(xPoints, yPoints, 8));
+
+        // draw a green octagon
+        g2D.setPaint(Color.green);
+        g2D.fill(scaledPolygon);
+
+        AffineTransform transform1 = new AffineTransform();
+        transform1.rotate(Math.toRadians(60));
+        transform1.translate(1, 2);
+        transform1.scale(2.0, 2.0);
+
+        Shape scaledPolygonRed = transform1.createTransformedShape(new Polygon(xPoints, yPoints, 8));
+
+        // draw a red octagon
+        g2D.setPaint(Color.red);
+        g2D.fill(scaledPolygonRed);
+
+        AffineTransform transform2 = new AffineTransform();
+        transform2.scale(2.0, 2.0);
+        transform2.rotate(Math.toRadians(60));
+        transform2.translate(1, 2);
+
+
+        Shape scaledPolygonBlue = transform2.createTransformedShape(new Polygon(xPoints, yPoints, 8));
+
+        // draw a red octagon
+        g2D.setPaint(Color.blue);
+        g2D.fill(scaledPolygonBlue);
     }
 }
